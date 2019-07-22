efe67eb61c7313089f5711c6ae59e1d3866690f2
Dan <dcollins31@gmail.com>
2019-07-22 13:53:58 -0500

    Connected MongoDB Atlas

--- server.js
+++ server.js
@@ -3,7 +3,12 @@
 //
 
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running!'));
 
